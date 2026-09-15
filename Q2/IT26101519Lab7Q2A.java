acebd4c2f32897ae0c4552fee7ee771e11359804
IT26101519 <IT26101519@my.sliit.lk>
2026-09-15 22:15:51 +0530

    Add files via upload

---
+++ Q2/IT26101519Lab7Q2A.java
@@ -0,0 +1,13 @@
+public class IT26101519Lab7Q2A { 
+    public static void main(String[] args) {
+        
+        // 4 rows
+        for(int i = 1; i <= 4; i++) {
+            // 5 columns with space
+            for(int j = 1; j <= 5; j++) {
+                System.out.print("$ ");
+            }
+            System.out.println(); // next line ku poganum
+        }
+    }
+}
